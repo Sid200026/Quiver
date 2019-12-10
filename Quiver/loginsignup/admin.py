11b8b027d8bbd5a722249f5efb80d14a12e1e6bc
Sid200026 <siddharthsingharoy@gmail.com>
2019-12-10 22:57:57 +0530

    Created the database for the users

--- Quiver/loginsignup/admin.py
+++ Quiver/loginsignup/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Beaver
 # Register your models here.
+
+admin.site.register(Beaver)
--- Quiver/loginsignup/models.py
+++ Quiver/loginsignup/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Beaver(models.Model):
+    gender_choice = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("N", "Cannot Specify"),
+    ]
+    user = models.ForeignKey(User, related_name="users", on_delete=models.CASCADE)
+    gender = models.CharField(
+        max_length=1,
+        choices=gender_choice,
+        default="Cannot Specify",
+    )
+    bio = models.TextField(help_text="Enter your profile bio")
+    date_of_birth = models.DateField(auto_now=False)
+    profile_photo = models.ImageField(upload_to="images/", help_text='Profile Photo')
+    phone = PhoneField(help_text='Contact phone number')
+
+    class Meta:
+        verbose_name_plural = "Beavers"
+
+    def __str__(self):
+        return f"Name:{self.user.username}" 
+    
+
