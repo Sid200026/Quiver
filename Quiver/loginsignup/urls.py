a08563ca90d1022d3a839081f63b51fcba2b932d
Sid200026 <siddharthsingharoy@gmail.com>
2019-12-20 11:40:41 +0530

    Added signup to url

--- Quiver/loginsignup/urls.py
+++ Quiver/loginsignup/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, re_path
-from .views import LoginView
+from .views import LoginView,SignUpView
 
 app_name = 'loginsignup'
 
 urlpatterns = [
     # path('', Landing, name="landing"),
     path('login/', LoginView.as_view(), name="login"),
-    # path('signup', SignUpView.as_view(), name="signup"),
+    path('signup', SignUpView.as_view(), name="signup"),
     # path('completeprofile', CompleteView.as_view(), name="complete"),
     # path('logout', LogoutUser, name="logout"),
 ]
