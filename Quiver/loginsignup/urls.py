dce794d7cf1527beac46942a325de44065aefa90
Sid200026 <siddharthsingharoy@gmail.com>
2019-12-21 20:30:07 +0530

    Added reset url

--- Quiver/loginsignup/urls.py
+++ Quiver/loginsignup/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
-from .views import LoginView, SignUpView, CompleteView, LogoutUser, Landing
+from .views import LoginView, SignUpView, CompleteView, ResetPassword, LogoutUser, Landing
 
 app_name = 'loginsignup'
 
 urlpatterns = [
     path('', Landing, name="landing"),
     path('login/', LoginView.as_view(), name="login"),
-    path('signup', SignUpView.as_view(), name="signup"),
-    path('completeprofile', CompleteView.as_view(), name="complete"),
-    path('logout', LogoutUser, name="logout"),
+    path('signup/', SignUpView.as_view(), name="signup"),
+    path('completeprofile/', CompleteView.as_view(), name="complete"),
+    path('reset/', ResetPassword.as_view(), name="reset" ),
+    path('logout/', LogoutUser, name="logout"),
 ]
