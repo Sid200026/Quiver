ff8e6ae2e7f62d991562603fec04f67442f8b599
Sid200026 <siddharthsingharoy@gmail.com>
2020-01-13 06:58:31 +0530

    Refactored Code

--- Quiver/loginsignup/static/loginsignup/JS/reset.js
+++ Quiver/loginsignup/static/loginsignup/JS/reset.js
@@ -1,9 +1,9 @@
 function validateForm() {
     const password = document.forms["resetForm"]["password"].value;
-    if(!password)
+    if (!password)
         return true;
     const confirmPassword = document.forms["resetForm"]["confirm"].value;
-    if (password!==confirmPassword) {
+    if (password !== confirmPassword) {
         document.getElementsByClassName("confirmpass")[0].innerHTML = "Passwords do not match";
         return false;
     }
--- Quiver/loginsignup/static/loginsignup/JS/signup.js
+++ Quiver/loginsignup/static/loginsignup/JS/signup.js
@@ -1,7 +1,7 @@
 function validateForm() {
     const password = document.forms["signupForm"]["password"].value;
     const confirmPassword = document.forms["signupForm"]["confirmpassword"].value;
-    if (password!==confirmPassword) {
+    if (password !== confirmPassword) {
         document.getElementsByClassName("confirmpass")[0].innerHTML = "Passwords do not match";
         return false;
     }
